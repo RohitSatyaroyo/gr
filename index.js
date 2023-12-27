2745a67187b27af5f12c5006ab21618b3d2a5817
RohitSatyaroyo <122684136+RohitSatyaroyo@users.noreply.github.com>
2023-12-27 09:21:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express=require('express')
-const cors=require('cors')
-const http=require('http')
+
+
 const app=express()
 
 app.use(express.json())
-app.use(cors())
-const mongoose=require('mongoose')
 
 
 
@@ -62,6 +60,7 @@ app.use('/api/auth',userRouter)
 
 
 
+app.listen(8080,()=>console.log('running'))
 
 
 
@@ -70,9 +69,3 @@ app.use('/api/auth',userRouter)
 
 
 
-
-mongoose.set("strictQuery",false)
-mongoose.connect('mongodb+srv://admin:12345@royoapi.3qmdrjq.mongodb.net/?retryWrites=true&w=majority').then(()=>{
-    console.log('database connected')
-  app.listen(8080,()=>console.log('running'))
-})
