28e3f99f7dfc541f8aa674b909d3e8a64d408f34
RohitSatyaroyo <122684136+RohitSatyaroyo@users.noreply.github.com>
2023-12-27 09:50:28 +0530

    Update userRoutes.js

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -2,6 +2,6 @@ const express=require('express')
 const userController=require('../Controller/UserController')
 const router=express.Router()
 
-router.get('/show',userController.show)
+router.get('/show',userController.getrec)
 
 module.exports=router
