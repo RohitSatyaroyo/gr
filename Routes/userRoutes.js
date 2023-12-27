6db9d3e2925d02445d3a816629bbd8784890da36
RohitSatyaroyo <122684136+RohitSatyaroyo@users.noreply.github.com>
2023-12-27 09:41:11 +0530

    Update userRoutes.js

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -2,14 +2,6 @@ const express=require('express')
 const userController=require('../Controller/UserController')
 const router=express.Router()
 
-router.post('/reg',userController.registeruser)
-
-router.post('/login',userController.authuser)
-
-router.get('/getAllUser/:id',userController.getAllUser)
-
-router.post('/sendmsg',userController.sendmsg)
-
-router.post('/getmsg',userController.getmsg)
+router.get('/show',userController.show)
 
 module.exports=router
