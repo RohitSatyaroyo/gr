dab42adb21402ae3923a6a010444cea63b42a36a
RohitSatyaroyo <122684136+RohitSatyaroyo@users.noreply.github.com>
2023-12-27 09:53:46 +0530

    Update UserController.js

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -3,7 +3,14 @@ const getrec=async(req,res)=>{
     const ds=await userModel.find()
     res.json(ds)
 }
+const registeruser=async(req,res)=>{
+    const {name,email,password}=req.body
+    // const hashedpassword=await bcrypt.hash(password,10)
+    const user=await userModel.create({name,email,password})
+    res.json(user)
+
+}
 
 
-module.exports={getrec}
+module.exports={getrec,registeruser}
     
