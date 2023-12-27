72d90e8ed9a78237587776d723260abb863eaf86
RohitSatyaroyo <122684136+RohitSatyaroyo@users.noreply.github.com>
2023-12-27 13:35:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const {Server}=require('socket.io')
 const server=http.createServer(app)
 const io=new Server(server,{
   cors:{
-     origin:["https://cha-fro.vercel.app/"],
+     origin:["https://cha-fro.vercel.app"],
      methods:["GET","POST"]
   
      
