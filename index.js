46876710fb9a0208eb6ad50a8d83488ec40c82a8
RohitSatyaroyo <122684136+RohitSatyaroyo@users.noreply.github.com>
2023-12-27 09:29:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express=require('express')
-
+const cors=require('cors')
+const http=require('http')
 
 const app=express()
+app.use(cors())
+const mongoose=require('mongoose')
+
 
 
 app.use(express.json())
