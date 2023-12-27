2d5beb351d068df2806589777a214bbce4e2735b
RohitSatyaroyo <122684136+RohitSatyaroyo@users.noreply.github.com>
2023-12-27 09:30:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const mongoose=require('mongoose')
 app.use(express.json())
 
 
+const {Server}=require('socket.io')
+const server=http.createServer(app)
+const io=new Server(server,{
+  cors:{
+     origin:["http://localhost:5173"]
+  
+     
+  },
+})
+
+
 
 
 
